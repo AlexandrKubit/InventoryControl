3923d44962348c0fb58f898a2cb2314d16915430
Александр Кубит <alexandr.kubit@yandex.ru>
2025-03-10 12:43:30 +0300

    убрал лишнее

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 namespace UI;
 
@@ -11,7 +10,6 @@ public class Program
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
-        //builder.Services.AddScoped(sp => new HttpClient(new CookieHandler()) { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
         await builder.Build().RunAsync();
     }
